8117996ceb74556d32defd6371db57076ad1fd4b
hanz <hi@hanz.dev>
2026-07-02 21:02:34 +0700

    feat: Added vite scaffold

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Transparency from "./pages/Transparency";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/transparency" element={<Transparency />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
---
+++ src/pages/Transparency.tsx
@@ -0,0 +1,13 @@
+function Transparency() {
+  return (
+    <pre>
+      {`TRANSPARENCY DASHBOARD
+
+This page will serve as the transparency dashboard for UKM Cyber Security ITS.
+
+Coming soon.`}
+    </pre>
+  );
+}
+
+export default Transparency;
